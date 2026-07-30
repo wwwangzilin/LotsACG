8ccf19d481a6851e71c73c33b8ade4986100684a
Acamer_ <wzl101@outlook.com>
2026-07-30 21:28:49 +0800

    fix: 修复帮助文档中的乱码问题

--- internal/interface/telegram/handlers/help.go
+++ internal/interface/telegram/handlers/help.go
@@ -17,29 +17,29 @@ func Help(ctx *telegohandler.Context, message telego.Message) error {
 	}
 	helpText := `使用方法:
 /setu - 随机图片(NSFW)
-/random - 随机全年龄图�?
+/random - 随机全年龄图片
 /search - 搜索相似图片
 /info - 发送作品图片和信息
 /files - 获取作品原图
 /hybrid - 混合搜索作品
 /similar - 搜索相似作品
-/recommend - 私聊中智能推荐作�?基于你喜�?不喜欢的标签偏好)
+/recommend - 私聊中智能推荐作品(基于你喜欢/不喜欢的标签偏好)
 `
 	helpText += `
 随机图片相关功能中支持使用以下格式的参数:
-使用 '|' 分隔'�?关系, 使用 '空格' 分隔'�?关系, 示例:
+使用 '|' 分隔'或'关系, 使用 '空格' 分隔'与'关系, 示例:
 
 /random 萝莉|白丝 猫耳|原创
 
-表示搜索包含"萝莉"�?白丝", 且包�?猫�?�?原创"的图�?
-Inline 查询(在任意聊天框中@本bot)支持同样的参数格�?
+表示搜索包含"萝莉"或"白丝", 且包含"猫耳"或"原创"的图片
+Inline 查询(在任意聊天框中@本bot)支持同样的参数格式
 `
 	isAdmin, _ := serv.IsAdminByTgID(ctx, message.From.ID)
 	if isAdmin {
 		helpText += `
-管理员命�?
-/addadmin - 添加管理�?
-/deladmin - 删除管理�?
+管理员命令:
+/addadmin - 添加管理员
+/deladmin - 删除管理员
 /delete - 删除整个作品
 /r18 - 设置作品R18标记
 /title - 设置作品标题
@@ -47,7 +47,7 @@ Inline 查询(在任意聊天框中@本bot)支持同样的参数格�?
 /autotag - 自动tag作品
 /addtags - 添加作品标签
 /deltags - 删除作品标签
-/tagalias - 为标签添加别�?
+/tagalias - 为标签添加别名
 /dump - 输出 json 格式作品信息
 /recaption - 重新生成作品描述
 /reindex - 重新索引作品
